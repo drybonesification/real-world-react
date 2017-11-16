ef8cefc7b59f184425d1aff16a41a4ea73cb33f6
8_n3z <jesus.saavedra37926637@gmail.com>
2017-11-16 10:31:24 -0700

    13 you can register now yay

--- src/index.js
+++ src/index.js
@@ -8,13 +8,15 @@ import store from "./store";
 import App from "./components/App";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import Register from "./components/Register"
 
 ReactDOM.render(
   <Provider store={store}>
     <Router history={hashHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={Home} />
-        {<Route path="/login" component={Login} />}
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
       </Route>
     </Router>
   </Provider>,
